4d30c78fe4c1df8506ea00900c1e1bffe12cf8a9
Kevin Pan <kevin@thenetcircle.com>
2014-04-22 15:33:04 +0800

    add jquery library

--- application/views/default.php
+++ application/views/default.php
@@ -5,6 +5,8 @@
   <title>The Netcircle - Translate Tool</title>
   <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+  <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
   <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
   <style>
   .break {
