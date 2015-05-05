2fdf5714b93be72266cd48873eb3f04f722f5cf2
Kevin Pan <kevin@thenetcircle.com>
2015-05-05 10:45:01 +0800

    quick fix error messages under php 5.6

--- application/config/mongo.php
+++ application/config/mongo.php
@@ -4,5 +4,5 @@ $config['mongo_server']  = 'mongodb://10.60.1.223:27018, moleman-poppen:27017';
 $config['mongo_dbname']  = 'meteor';
 $config['mongo_options'] = array('replicaSet' => 'rs1');
 
-$config['poppen_collection'] = 'PoppenMessage_MigrationTest';
-$config['gays_collection']   = 'GaysMessage_MigrationTest';
+$config['poppen_collection'] = 'PoppenMessage';
+$config['gays_collection']   = 'GaysMessage';
